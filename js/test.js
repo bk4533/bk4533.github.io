f59b39d59634e58d25ef4fb149724ba952209d5e
bk4533 <125331844+bk4533@users.noreply.github.com>
2023-04-04 12:51:26 +0900

    aaa

--- js/test.js
+++ js/test.js
@@ -1,3 +1,16 @@
 $(function(){
     $('.skills').myskillbar({});
+
+    let height;
+    $(window).scroll(function(){
+        const oheight = $('#header').offset().top;
+        console.log(oheight);
+        if(oheight > 0){
+            $('#header').css('background','rgba(228,228,228,0.5)')
+        }else{
+            $('#header').css('background','rgb(228,228,228)')
+        }
+
+
+    });
 });
